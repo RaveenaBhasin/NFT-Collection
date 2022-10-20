166811bd24b7a2883a9c95d351141ba7567da50d
RaveenaBhasin <raveenabhasin15@gmail.com>
2022-10-21 00:10:05 +0530

    feat: add cryptodev smart contract

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5B8c9Fbe1CC9CF7Fe0Bf4b107aEcd2C990C17D3B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,24 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  console.log("Crypto Devs Contract Address:", deployedCryptoDevsContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
